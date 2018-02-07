77d4e54c84730a8154b45695e32660700f509901
Arloz55 <31456588+Arloz55@users.noreply.github.com>
2018-02-06 23:04:02 -0600

    MP3-Player 1
    Prototype from mp3 player #1

--- source.js
+++ source.js
@@ -1,247 +1,335 @@
+var m=0;
+var m1=0;
+var m2 = 3;
+var start=0;
+var dur=2;
+var radius = 200;
+var mv ;
+var analyzer;
+var analyzer2;
+var analyzer3;
+var el;
 
-var osc;
-var env;
-var slider;
-var canv;
 
-function setup(){
-    canv = createCanvas(200,200);
-    canv.position(500,500)
-    background(128);
-
-    slider = createSlider(0,5,2.5,0);
-    slider.position(60, 590);
-    slider.style('width', '200px');
-
-    env = new p5.Env(0.01,0.2,5,0.5);
-
-    osc= new p5.Oscillator();
-    osc.amp(env);
-    osc.freq(800);
-    osc.setType('square');
-    osc.start();
+function preload() {
+  soundFormats('mp3');
+  mySound = loadSound('Initialization Sequence.mp3');
+  mySound2 = loadSound('Trapped.mp3');
+  mySound3 = loadSound('United.mp3');
 }
 
-function keyPressed(){
+function setup() {
+
+  sel = createSelect();
+  sel.position(650, 177);
+  sel.option('Elija una Canción');
+  sel.option('Initialization Sequence');
+  sel.option('Trapped');
+  sel.option('United');
+  sel.changed(mySelectEvent);
+
+  button = createButton('Play');
+  button.position(700, 258);
+  button.mousePressed(mouseclicked);
+
+  button2 = createButton('Pause');
+  button2.position(740, 258);
+  button2.mousePressed(mouseclicked2);
+
+  button3 = createButton('Stop');
+  button3.position(720, 288);
+  button3.mousePressed(mouseclicked3);
+
+  button5 = createButton('random');
+  button5.position(870, 288);
+  button5.mousePressed(mouseclicked5);
+
+  button5 = createButton('Next');
+  button5.position(815, 258);
+  button5.mousePressed(mouseclicked6);
+
+  button6 = createButton('Prev');
+  button6.position(630, 258);
+  button6.mousePressed(mouseclicked7);
+
+  button4 = createButton('loop');
+  button4.position(967, 288);
+  button4.mousePressed(mouseclicked4);
+
+  vslider2 = createSlider(0, 100,50);
+  vslider2.position(640 , 350);
+  vslider2.style('width', '200px');
+
+//
+  
+  var cnv = createCanvas(200,200);
+  cnv.position(370, 400);
+  
+  
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySound);
+  analyzer2 = new p5.Amplitude();
+  analyzer2.setInput(mySound2);
+  analyzer3 = new p5.Amplitude();
+  analyzer3.setInput(mySound3);
+}
+
+function draw(){
+
+  mv = masterVolume(vslider2.value()/100);
+  background(55,125,34);
+ 
+  var rms = analyzer.getLevel();
+  var rms1 = analyzer2.getLevel();
+  var rms2 = analyzer3.getLevel();
+  fill(190,0,14);
+  stroke(0);
+
+  el=ellipse(100, 100, 10+(rms+rms1+rms2)*300, 10+(rms+rms1+rms2)*300 );
+
+}
+
+function mySelectEvent() {
+
+  mySound.stop();
+  mySound2.stop();
+  mySound3.stop();
+
+  var item = sel.value();
+
+ switch(item){
+
+case 'Initialization Sequence' :
+console.log("good");
+m=1;
+break;
+
+case 'Trapped':
+console.log("good 2");
+m=2;
+break;
+
+case 'United':
+console.log("good 3");
+m=3;
+break;
+
+default: m=0;
+
+
+}
+}
+
+function mouseclicked(){
+
+switch(m){
+
+  case 1:
+  console.log("primero");
+  mySound.play();
+break;
+case 2:
+console.log("segundo");
+mySound2.play();
+break;
+case 3:
+console.log("tercero");
+mySound3.play();
+break;
+default: console.log("No pista");
+
+}
+}
+
+function mouseclicked2(){
+
+
+  switch(m){
+  
+    case 1:
+    console.log("primero");
+    mySound.pause();
+  break;
+  case 2:
+  console.log("segundo");
+  mySound2.pause();
+  break;
+  case 3:
+  console.log("tercero");
+  mySound3.pause();
+  break;
+  default: console.log("No pista");
+  
+  }
+  }
+
+  function mouseclicked3(){
+
+
+    switch(m){
     
-    var fr = 0;
-    switch(key){
-
-        case "Q":
-        console.log("C5");
-        fr = 523.25;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "2":
-        console.log("C#5");
-        fr = 554.36;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "W":
-        console.log("D5");
-        fr = 587.33;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "3":
-        console.log("D#5");
-        fr = 622.25;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "E":
-        console.log("E5");
-        fr = 659.25;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "R":
-        console.log("F5");
-        fr = 698.45;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "5":
-        console.log("F#5");
-        fr = 740;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "T":
-        console.log("G5");
-        fr = 784;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "6":
-        console.log("G#5");
-        fr = 830.60;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "Y":
-        console.log("A5");
-        fr = 880;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "7":
-        console.log("A#5");
-        fr = 932.32;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "U":
-        console.log("B5");
-        fr = 987.76;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "I":
-        console.log("C6");
-        fr = 1046.50;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "9":
-        console.log("C#6");
-        fr = 1108.73;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "O":
-        console.log("D6");
-        fr = 1174.66;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "0":
-        console.log("D#6");
-        fr = 1244.51;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "P":
-        console.log("E6");
-        fr = 1318.51;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "Z":
-        console.log("C4");
-        fr = 261.63;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "S":
-        console.log("C#4");
-        fr = 277.18;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "X":
-        console.log("D4");
-        fr = 293.66;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "D":
-        console.log("D#4");
-        fr = 311.12;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "C":
-        console.log("E4");
-        fr = 329.63;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "V":
-        console.log("F4");
-        fr = 349.23;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "G":
-        console.log("F#4");
-        fr = 370;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "B":
-        console.log("G4");
-        fr = 392;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "H":
-        console.log("G#4");
-        fr = 415.3;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "N":
-        console.log("A4");
-        fr = 440;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
-        case "J":
-        console.log("A#4");
-        fr = 466.16;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-        
-        case "M":
-        console.log("B4");
-        fr = 493.88;
-        fill(240);
-        ellipse(100,100,100,100);
-        break;
-
+    case 1:
+    console.log("primero");
+    mySound.stop();
+    break;
+    case 2:
+    console.log("segundo");
+    mySound2.stop();
+    break;
+    case 3:
+    console.log("tercero");
+    mySound3.stop();
+    break;
+    default: console.log("No pista");
+    
+    }
     }
 
-    osc.freq(fr);
-    env.triggerAttack();
-    var valSlider = slider.value();
-    env.mult(valSlider);
+    function mouseclicked4(){
+      mySound.stop();
+      mySound2.stop();
+      mySound3.stop();
 
-}
+      switch(m){
+      
+        case 1:
+        console.log("primero");
+        mySound.loop(1,1,1,start,dur);
+      break;
+      case 2:
+      console.log("segundo");
+      mySound2.loop(1,1,1,start,dur);
+      break;
+      case 3:
+      console.log("tercero");
+      mySound3.loop(1,1,1,start,dur);
+      break;
+      default: console.log("No pista");
+      
+      
+      }
+      
+      }
+    
+      function mouseclicked5(){
 
+        mySound.stop();
+        mySound2.stop();
+        mySound3.stop();
+        var n = Math.floor(Math.random()*3) +1 ; 
 
-function keyReleased(){
-    env.triggerRelease();
-    background(128);
+        console.log(n);
 
-}
+        switch(n){
+        
+        case 1:
+        console.log("primero");
+        mySound.play();
+        sel.value('Initialization Sequence')
+        m=1;
+
+        break;
+        case 2:
+        console.log("segundo");
+        mySound2.play();
+        sel.value('Trapped')
+        m=2;
+
+        break;
+        case 3:
+        console.log("tercero");
+        mySound3.play();
+        sel.value('United')
+        m=3;
+
+        break;
+        default: console.log("No pista");
+        
+        }
+        }
+
+        function mouseclicked6(){
+
+          mySound.stop();
+          mySound2.stop();
+          mySound3.stop();
+          m1= m1 +1;
+  
+          console.log(m1);
+  
+          switch(m1){
+          
+        
+          case 1:
+          console.log("primero");
+          mySound.play();
+          sel.value('Initialization Sequence')
+          m=1;
+          
+  
+          break;
+          case 2:
+          console.log("segundo");
+          mySound2.play();
+          sel.value('Trapped')
+          m=2;
+          
+  
+          break;
+          case 3:
+          console.log("tercero");
+          mySound3.play();
+          sel.value('United')
+          m1=0;
+          m=3;
+  
+          break;
+          default : m =0;
+ 
+          }
+          }
+
+        function mouseclicked7(){
+
+          
+          m2= m2 -1;
+
+          mySound.stop();
+          mySound2.stop();
+          mySound3.stop();
+  
+          console.log(m2);
+  
+          switch(m2){
+          
+        
+
+          case 1:
+          console.log("primero");
+          mySound.play();
+          sel.value('Initialization Sequence')
+          m=1;
+          m2=4;
+          
+  
+          break;
+          case 2:
+          console.log("segundo");
+          mySound2.play();
+          sel.value('Trapped')
+          m=2;
+          
+  
+          break;
+
+          case 3:
+          console.log("tercero");
+          mySound3.play();
+          sel.value('United')
+         
+          m=3;
+  
+          break;          
+          }
+          }
+
